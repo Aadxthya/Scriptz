8b1e4c0a1fe788b472e595e569403185fee7d8a4
Aadithya <aadithyaeaswaran@gmail.com>
2025-01-24 22:33:44 +0530

    day 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,12 @@
 import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ui/ModeToggle";
 import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
-
-import Image from "next/image";
 
 export default function Home() {
   return (
     <ClerkProvider>
       <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <Button variant={"secondary"}>Click me</Button>
-    </div>
-    <ModeToggle/>
-
+        <h1>home page content</h1>
+        </div>
     </ClerkProvider>
   );
 }
